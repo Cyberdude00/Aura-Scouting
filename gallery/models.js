965194cf05d029ea29b924d81df49d755682733a
Gerardo Quintero <gera.quinteros@hotmail.com>
2025-05-16 18:40:23 -0700

    added a gdrive example to model

--- gallery/models.js
+++ gallery/models.js
@@ -1,5 +1,15 @@
 const models = [
   {
+    name: "Prueba Gdrive",
+    image: "https://lh3.googleusercontent.com/d/1s_4QEMYSR6ALJiQRdspG18yJ32NhCouY",
+    height: "186 cm",
+    measurements: "90-75-92",
+    gallery: [
+      "https://lh3.googleusercontent.com/d/1KLArJL-c7UzY_d2wAPrfWJC4zPC3w1OF",
+      "https://lh3.googleusercontent.com/d/1UVyPZCO_bDV3pchKl3YjURd-dAce4AIC"
+    ]
+  },
+  {
     name: "Alex Rivera",
     image: "https://res.cloudinary.com/djpi5yzlu/image/upload/v1747106010/c57593e2-86df-488a-b33b-ce773f4e759c_m21gmv.jpg",
     height: "186 cm",
@@ -75,7 +85,7 @@ const models = [
       "https://res.cloudinary.com/djpi5yzlu/image/upload/v1747106010/c57593e2-86df-488a-b33b-ce773f4e759c_m21gmv.jpg"
     ]
   }
-  
-  
+
+
   // Aquí puedes agregar más modelos si lo necesitas
 ];
